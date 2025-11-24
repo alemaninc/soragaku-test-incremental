e54398de0367d330437030fc1fd28f6eacbc0729
alemaninc <97172850+alemaninc@users.noreply.github.com>
2025-11-24 15:50:25 +0000

    Fix syntax errors

--- test1.js
+++ test1.js
@@ -1,27 +1,28 @@
 const player = {
-item: 0,
-generators: 0,
-gultiplier:0,
-// i am not explaing this
-upgrade: 1,
-gcost: 10,
-//gcost means generator cost
-ucost: 50,
-//ucost means upgrade cost
+	item: 0,
+	generators: 0,
+	gultiplier:0,
+	// i am not explaing this
+	upgrade: 1,
+	gcost: 10,
+	//gcost means generator cost
+	ucost: 50,
+	//ucost means upgrade cost
 }
 function click() {
 	player.item++;
 }
-function addgenerator(player.item, player.gcost) {
+function addgenerator() {
 	if(player.item >= player.gcost) {
 		player.item-=player.gcost;
 	player.gcost+=player.gcost;
 	player.generators++
 	}
 }
-	function addupgrade(player.item, player.ucost) {
+function addupgrade() {
 	if(player.item >= player.ucost) {
 		player.item-=player.ucost;
+	}
 	player.ucost+=player.ucost;
 	player.upgrade++
-	}
+}
